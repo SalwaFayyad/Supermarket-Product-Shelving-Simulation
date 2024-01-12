7564bde6632b20472d2ce4662d30de99315ae621
saharfayyad02 <saharfayyad02@gmail.com>
2024-01-12 20:37:48 +0200

    adding comments

--- main.c
+++ main.c
@@ -516,7 +516,7 @@ void drawTime() {
     int min = minutes;
     char minStr[5];
     sprintf(minStr, "%d", min);
-    for (int i = 0; secondsStr[i] != '\0'; ++i) {
+    for (int i = 0; minStr[i] != '\0'; ++i) {
         /* Draw a character using the GLUT_BITMAP_HELVETICA_18 font at the current raster position */
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, minStr[i]);
     }
